bfae73a404551729a38e02709ee7824f202449a3
Pmsmm <pedro8745@gmail.com>
2022-01-21 17:30:56 +0000

    Created the Readme file with all the relevant information about the interaction. Added the source code and enclosure files as well as some images showing the wiring diagram and assembled interaction.

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Light-Sensor-Interaction
+# Light Sensor Interaction
+
+The Light Sensor Interaction is a mini game that was developed for my Master Thesis "Developing a Support Infrastructure for an Escape The Room and Peddy Paper Games".
+
+This mini game, or interaction as I so call it, was developed to be part of an Escape the Room game that was also developed as part of my thesis. Since this interaction can work as a standalone mini game I decided to place it here for anyone interested in using it.
+
+## List of Components
+
+- LDR - Light Controlled Resistor (1x);
+- 220Ω Resistor (1x);
+- Arduino Nano (1x);
+
+![Wiring Diagram](images/WiringDiagram.jpg)
+
+In order to fully assemble the Interaction, so as to look like the picture below, you will also need to 3D Print the enclosure which is divided into two parts that can be found [here](enclosure/).
+
+![Assembled Interaction](images/AssembledInteraction.jpg)
+
+## The Purpose of The Game
+
+There is actually not a lot to say about the Light Sensor Interaction. When it was integrated in the Escape The Room this interaction served the main purpose of being activated by a flashlight that the players had to obtain by solving other mini games. It is still a very good way of teaching how LDR sensors work and creating playful experiences using this interaction.
+
+## Instructions
+
+First start by uploading the code to your Arduino Nano (This is the one I used so I can only guarantee proper working with this micro-controller).
+
+### Starting the Game
+
+To start the game you must first type in the Arduino IDE serial monitor the following:
+
+- > COM:START;ID:123456789
+
+This command will then print in the Serial Monitor the solution of the game which then needs to be introduced using the keypad. If the introduced code is correct then the following message will appear in the serial monitor:
+
+- > COM:INTERACTION_SOLVED;MSG:User Iluminated LDR Sensor;PNT:1750
+
+## Warning
+
+The source code for this interaction contains a lot of logic that was made to communicate with the infrastructure that was developed for my thesis and therefor it is not as clean as it could be when compared to a mini game that is developed with the intent of being used by the Arduino IDE directly.
---
+++ src/LightSensorInteraction.ino
@@ -0,0 +1,149 @@
+const uint8_t LDR_Pin = A0;
+uint16_t LDR_Value = 0;
+
+bool INTERACTION_SOLVED, INTERACTION_RUNNING;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (!Serial) {
+    Serial.begin(9600);
+  }
+  if (Serial.available()) {
+    processSerialMessage();
+  }
+  if (INTERACTION_SOLVED == false && INTERACTION_RUNNING == true) {
+    gameLoop();
+  }
+}
+
+void gameLoop() {
+  LDR_Value = map(analogRead(LDR_Pin), 0, 1023, 0, 9);
+  Serial.println(LDR_Value);
+  Serial.println(analogRead(LDR_Pin));
+  if (LDR_Value == 9) {
+    checkWinning();
+  }
+}
+
+void processSerialMessage() {
+  const uint8_t BUFF_SIZE = 64; // make it big enough to hold your longest command
+  static char buffer[BUFF_SIZE + 1]; // +1 allows space for the null terminator
+  static uint8_t length = 0; // number of characters currently in the buffer
+
+  char c = Serial.read();
+  if ((c == '\r') || (c == '\n')) {
+    // end-of-line received
+    if (length > 0) {
+      tokenizeReceivedMessage(buffer);
+    }
+    length = 0;
+  } else {
+    if (length < BUFF_SIZE) {
+      buffer[length++] = c; // append the received character to the array
+      buffer[length] = 0; // append the null terminator
+    }
+  }
+}
+
+void tokenizeReceivedMessage(char *msg) {
+  const uint8_t COMMAND_PAIRS = 10;
+  char* tokenizedString[COMMAND_PAIRS + 1];
+  uint8_t index = 0;
+
+  char* command = strtok(msg, ";");
+  while (command != 0) {
+    char* separator = strchr(command, ':');
+    if (separator != 0) {
+      *separator = 0;
+      tokenizedString[index++] = command;
+      ++separator;
+      tokenizedString[index++] = separator;
+    }
+    command = strtok(0, ";");
+  }
+  tokenizedString[index] = 0;
+
+  processReceivedMessage(tokenizedString);
+}
+
+void processReceivedMessage(char** command) {
+  if (strcmp(command[1], "START") == 0) {
+    startSequence(command[3]);
+  } else if (strcmp(command[1], "PAUSE") == 0) {
+    pauseSequence(command[3]);
+  } else if (strcmp(command[1], "STOP") == 0) {
+    stopSequence(command[3]);
+  } else if (strcmp(command[1], "INTERACTION_SOLVED_ACK") == 0) {
+    setInteractionSolved();
+  } else if (strcmp(command[1], "PING") == 0) {
+    ping(command[3]);
+  } else if (strcmp(command[1], "BAUD") == 0) {
+    setBaudRate(atoi(command[3]), command[5]);
+  } else if (strcmp(command[1], "SETUP") == 0) {
+    Serial.println("COM:SETUP;INT_NAME:Light Sensor Interaction;BAUD:9600");
+    Serial.flush();
+  }
+}
+
+//TODO: Review This Method once Interaction Is Completed
+void startSequence(char* TIMESTAMP) {
+  INTERACTION_SOLVED = false;
+  INTERACTION_RUNNING = true;
+  Serial.print("COM:START_ACK;MSG:Interaction Started Successfully");
+  Serial.print(";ID:");
+  Serial.print(TIMESTAMP);
+  Serial.print("\r\n");
+  Serial.flush();
+}
+
+void pauseSequence(char* TIMESTAMP) {
+  INTERACTION_RUNNING = !INTERACTION_RUNNING;
+  if (INTERACTION_RUNNING) {
+    Serial.print("COM:PAUSE_ACK;MSG:Device is now running;ID:");
+  } else {
+    Serial.print("COM:PAUSE_ACK;MSG:Device is now paused;ID:");
+  }
+  Serial.print(TIMESTAMP);
+  Serial.print("\r\n");
+  Serial.flush();
+}
+
+void stopSequence(char* TIMESTAMP) {
+  INTERACTION_RUNNING = false;
+  Serial.print("COM:STOP_ACK;MSG:Device is now stopped;ID:");
+  Serial.print(TIMESTAMP);
+  Serial.print("\r\n");
+  Serial.flush();
+}
+
+void setInteractionSolved() {
+  INTERACTION_SOLVED = true;
+  INTERACTION_RUNNING = false;
+}
+
+void ping(char* TIMESTAMP) {
+  Serial.print("COM:PING;MSG:PING;ID:");
+  Serial.print(TIMESTAMP);
+  Serial.print("\r\n");
+  Serial.flush();
+}
+
+void setBaudRate(int baudRate, char* TIMESTAMP) {
+  Serial.flush();
+  Serial.begin(baudRate);
+  Serial.print("COM:BAUD_ACK;MSG:The Baud Rate was set to ");
+  Serial.print(baudRate);
+  Serial.print(";ID:");
+  Serial.print(TIMESTAMP);
+  Serial.print("\r\n");
+  Serial.flush();
+}
+
+bool checkWinning() {
+  Serial.println("COM:INTERACTION_SOLVED;MSG:User Iluminated LDR Sensor;PNT:1750");
+  Serial.flush();
+  INTERACTION_SOLVED = true;
+}
